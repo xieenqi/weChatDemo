45f6c31e050aecaa2dde673575a5a20eb9bcd828
juno <329191055@qq.com>
2018-06-08 14:14:37 +0600

    新加入功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,10 +7,18 @@ Page({
     motto: 'Hello World欢迎来到！！！！',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    custom: "234567那个狗食不果腹你搜公告说过没干什么民事诉讼所付过所十几个金融楷模"
+  
   },
+  //button的点击事件
+  click1: function (event) {
+    this.setData({ custom: "点击过后设置的文字111111111111111111115555555555555555555" })
+    console.log(event)
+  },
+
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -21,7 +29,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -43,7 +51,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
